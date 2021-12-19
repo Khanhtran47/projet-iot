40bd2b846e4abe964ffb103418fb60f5f8aecf2f
Khanhtran47 <tranduckhanh23@gmail.com>
2021-12-20 00:55:08 +0100

    change databse config to the Heroku databse

--- database/config.php
+++ database/config.php
@@ -1,8 +1,11 @@
 <?php
-$currency = '$';
-$db_username = 'root';
-$db_password = '';
-$db_name = 'bolt';
-$db_host = 'localhost';
-$mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
-?>
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"], 1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
