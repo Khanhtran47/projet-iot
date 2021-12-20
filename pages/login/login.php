281ef766fcb95d892a63a9f126fa4fbed458c2a0
Khanhtran47 <tranduckhanh23@gmail.com>
2021-12-20 04:11:32 +0100

    edit files

--- pages/login/login.php
+++ pages/login/login.php
@@ -28,9 +28,9 @@ if (isset($_SESSION["username"])) {
   <link rel="preconnect" href="https://fonts.gstatic.com" />
   <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600;700&display=swap" rel="stylesheet" />
 
-  <link rel="stylesheet" href="../../css/style.css" />
+  <link rel="stylesheet" href="../../css/grid.css" />
 
-  <link rel="stylesheet" href="../../css/general.css" />
+  <link rel="stylesheet" href="login.css" />
 
 
   <title>High Knowledge Technology</title>
@@ -66,111 +66,142 @@ if (isset($_SESSION["username"])) {
     </button>
   </header>
 
-  <section class="section-cta" id="cta">
-    <div class="container">
-      <div class="cta">
-        <div class="cta-text-box">
-          <h2 class="heading-secondary">Join us now</h2>
-          <p class="cta-text">
-            Start building PC today. We can help you anytime.
-          </p>
-
-          <form method="POST" class="cta-form" action="../../components/verify.php" netlify>
-            <div>
-              <label for="right-label">Email</label>
-              <input id="right-label" type="email" name="username" required />
-            </div>
-
-            <div>
-              <label for="right-label">Password</label>
-              <input id="right-label" type="password" name="pwd" required />
-            </div>
-            <input type="submit" id="right-label" value="Login" class="btn btn--form">
-            <input type="reset" id="right-label" value="Reset" class="btn btn--form">
-
-          </form>
+  <div class="container-form" id="container-form">
+    <div class="form-container sign-up-container">
+      <form action="#">
+        <h1>Create Account</h1>
+        <div class="social-container">
+          <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+          <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+          <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
         </div>
-        <div class="cta-img-box" role="img" aria-label="Woman enjoying food"></div>
+        <span>or use your email for registration</span>
+        <input type="text" placeholder="Name" />
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button>Sign Up</button>
+      </form>
+    </div>
+    <div class="form-container sign-in-container">
+      <form action="#">
+        <h1>Sign in</h1>
+        <div class="social-container">
+          <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
+          <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
+          <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+        </div>
+        <span>or use your account</span>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <a href="#">Forgot your password?</a>
+        <button>Sign In</button>
+      </form>
+    </div>
+    <div class="overlay-container">
+      <div class="overlay">
+        <div class="overlay-panel overlay-left">
+          <h1>Welcome Back!</h1>
+          <p>To keep connected with us please login with your personal info</p>
+          <button class="ghost" id="signIn">Sign In</button>
+        </div>
+        <div class="overlay-panel overlay-right">
+          <h1>Hello, Friend!</h1>
+          <p>Enter your personal details and start journey with us</p>
+          <button class="ghost" id="signUp">Sign Up</button>
+        </div>
       </div>
     </div>
-  </section>
-
-
-  <div class="row" style="margin-top:10px;">
-    <div class="small-12">
-      <footer class="footer">
-        <div class="container grid grid--footer">
-          <div class="logo-col">
-            <a href="#" class="footer-logo">
-              <img class="logo" alt="HKGT logo" src="#" />
-            </a>
-
-            <ul class="social-links">
-              <li>
-                <a class="footer-link" href="#">
-                  <ion-icon class="social-icon" name="logo-instagram"></ion-icon>
-                </a>
-              </li>
-              <li>
-                <a class="footer-link" href="#">
-                  <ion-icon class="social-icon" name="logo-facebook"></ion-icon>
-                </a>
-              </li>
-              <li>
-                <a class="footer-link" href="#">
-                  <ion-icon class="social-icon" name="logo-twitter"></ion-icon>
-                </a>
-              </li>
-            </ul>
-
-            <p class="copyright">
-              Copyright &copy; <span class="year">2027</span> by HKT, Inc. All
-              rights reserved.
-            </p>
-          </div>
-
-          <div class="address-col">
-            <p class="footer-heading">Contact us</p>
-            <address class="contacts">
-              <p class="address">15 avenue Maréchal Foch, 41000 Blois, France</p>
-              <p>
-                <a class="footer-link" href="tel:+33 123 45 67 89">+33 123 45 67 89</a><br />
-                <a class="footer-link" href="mailto:hungc4k43@gmail.com">hungc4k43@gmail.com</a>
-              </p>
-            </address>
-          </div>
-
-          <nav class="nav-col">
-            <p class="footer-heading">Account</p>
-            <ul class="footer-nav">
-              <li><a class="footer-link" href="#">Create account</a></li>
-              <li><a class="footer-link" href="#">Sign in</a></li>
-              <li><a class="footer-link" href="#">iOS app</a></li>
-              <li><a class="footer-link" href="#">Android app</a></li>
-            </ul>
-          </nav>
-
-          <nav class="nav-col">
-            <p class="footer-heading">Company</p>
-            <ul class="footer-nav">
-              <li><a class="footer-link" href="#">About HKT</a></li>
-              <li><a class="footer-link" href="#">For Business</a></li>
-              <li><a class="footer-link" href="#">Partners</a></li>
-              <li><a class="footer-link" href="#">Careers</a></li>
-            </ul>
-          </nav>
-
-          <nav class="nav-col">
-            <p class="footer-heading">Resources</p>
-            <ul class="footer-nav">
-              <li><a class="footer-link" href="#">Help center</a></li>
-              <li><a class="footer-link" href="#">Privacy & terms</a></li>
-            </ul>
-          </nav>
-        </div>
-      </footer>
-    </div>
   </div>
+
+
+  <footer class="footer">
+    <div class="container grid grid--footer">
+      <div class="logo-col">
+        <a href="#" class="footer-logo">
+          <img class="logo" alt="HKGT logo" src="#" />
+        </a>
+
+        <ul class="social-links">
+          <li>
+            <a class="footer-link" href="#">
+              <ion-icon class="social-icon" name="logo-instagram"></ion-icon>
+            </a>
+          </li>
+          <li>
+            <a class="footer-link" href="#">
+              <ion-icon class="social-icon" name="logo-facebook"></ion-icon>
+            </a>
+          </li>
+          <li>
+            <a class="footer-link" href="#">
+              <ion-icon class="social-icon" name="logo-twitter"></ion-icon>
+            </a>
+          </li>
+        </ul>
+
+        <p class="copyright">
+          Copyright &copy; <span class="year">2027</span> by HKT, Inc. All rights reserved.
+        </p>
+      </div>
+
+      <div class="address-col">
+        <p class="footer-heading">Contact us</p>
+        <address class="contacts">
+          <p class="address">15 avenue Maréchal Foch, 41000 Blois, France</p>
+          <p>
+            <a class="footer-link" href="tel:+33 123 45 67 89">
+              +33 123 45 67 89
+            </a>
+            <br />
+            <a class="footer-link" href="mailto:hungc4k43@gmail.com">
+              hungc4k43@gmail.com
+            </a>
+          </p>
+        </address>
+      </div>
+
+      <nav class="nav-col">
+        <p class="footer-heading">Account</p>
+        <ul class="footer-nav">
+          <li><a class="footer-link" href="#">Create account</a></li>
+          <li><a class="footer-link" href="#">Sign in</a></li>
+          <li><a class="footer-link" href="#">iOS app</a></li>
+          <li><a class="footer-link" href="#">Android app</a></li>
+        </ul>
+      </nav>
+
+      <nav class="nav-col">
+        <p class="footer-heading">Company</p>
+        <ul class="footer-nav">
+          <li><a class="footer-link" href="#">About HKT</a></li>
+          <li><a class="footer-link" href="#">For Business</a></li>
+          <li><a class="footer-link" href="#">Partners</a></li>
+          <li><a class="footer-link" href="#">Careers</a></li>
+        </ul>
+      </nav>
+
+      <nav class="nav-col">
+        <p class="footer-heading">Resources</p>
+        <ul class="footer-nav">
+          <li><a class="footer-link" href="#">Help center</a></li>
+          <li><a class="footer-link" href="#">Privacy & terms</a></li>
+        </ul>
+      </nav>
+    </div>
+  </footer>
+  <script type="text/javascript">
+    const signUpButton = document.getElementById('signUp');
+    const signInButton = document.getElementById('signIn');
+    const container = document.getElementById('container-form');
+
+    signUpButton.addEventListener('click', () => {
+      container.classList.add("right-panel-active");
+    });
+
+    signInButton.addEventListener('click', () => {
+      container.classList.remove("right-panel-active");
+    });
+  </script>
 </body>
 
 </html>
--- pages/register/register.php
+++ pages/register/register.php
@@ -121,82 +121,81 @@ if (isset($_SESSION["username"])) {
   </section>
 
 
-  <div class="row" style="margin-top:10px;">
-    <div class="small-12">
+  <footer class="footer">
+    <div class="container grid grid--footer">
+      <div class="logo-col">
+        <a href="#" class="footer-logo">
+          <img class="logo" alt="HKGT logo" src="#" />
+        </a>
 
-      <footer class="footer">
-        <div class="container grid grid--footer">
-          <div class="logo-col">
-            <a href="#" class="footer-logo">
-              <img class="logo" alt="HKGT logo" src="#" />
+        <ul class="social-links">
+          <li>
+            <a class="footer-link" href="#">
+              <ion-icon class="social-icon" name="logo-instagram"></ion-icon>
             </a>
+          </li>
+          <li>
+            <a class="footer-link" href="#">
+              <ion-icon class="social-icon" name="logo-facebook"></ion-icon>
+            </a>
+          </li>
+          <li>
+            <a class="footer-link" href="#">
+              <ion-icon class="social-icon" name="logo-twitter"></ion-icon>
+            </a>
+          </li>
+        </ul>
 
-            <ul class="social-links">
-              <li>
-                <a class="footer-link" href="#">
-                  <ion-icon class="social-icon" name="logo-instagram"></ion-icon>
-                </a>
-              </li>
-              <li>
-                <a class="footer-link" href="#">
-                  <ion-icon class="social-icon" name="logo-facebook"></ion-icon>
-                </a>
-              </li>
-              <li>
-                <a class="footer-link" href="#">
-                  <ion-icon class="social-icon" name="logo-twitter"></ion-icon>
-                </a>
-              </li>
-            </ul>
+        <p class="copyright">
+          Copyright &copy; <span class="year">2027</span> by HKT, Inc. All rights reserved.
+        </p>
+      </div>
 
-            <p class="copyright">
-              Copyright &copy; <span class="year">2027</span> by HKT, Inc. All
-              rights reserved.
-            </p>
-          </div>
+      <div class="address-col">
+        <p class="footer-heading">Contact us</p>
+        <address class="contacts">
+          <p class="address">15 avenue Maréchal Foch, 41000 Blois, France</p>
+          <p>
+            <a class="footer-link" href="tel:+33 123 45 67 89">
+              +33 123 45 67 89
+            </a>
+            <br />
+            <a class="footer-link" href="mailto:hungc4k43@gmail.com">
+              hungc4k43@gmail.com
+            </a>
+          </p>
+        </address>
+      </div>
 
-          <div class="address-col">
-            <p class="footer-heading">Contact us</p>
-            <address class="contacts">
-              <p class="address">15 avenue Maréchal Foch, 41000 Blois, France</p>
-              <p>
-                <a class="footer-link" href="tel:+33 123 45 67 89">+33 123 45 67 89</a><br />
-                <a class="footer-link" href="mailto:hungc4k43@gmail.com">hungc4k43@gmail.com</a>
-              </p>
-            </address>
-          </div>
+      <nav class="nav-col">
+        <p class="footer-heading">Account</p>
+        <ul class="footer-nav">
+          <li><a class="footer-link" href="#">Create account</a></li>
+          <li><a class="footer-link" href="#">Sign in</a></li>
+          <li><a class="footer-link" href="#">iOS app</a></li>
+          <li><a class="footer-link" href="#">Android app</a></li>
+        </ul>
+      </nav>
 
-          <nav class="nav-col">
-            <p class="footer-heading">Account</p>
-            <ul class="footer-nav">
-              <li><a class="footer-link" href="#">Create account</a></li>
-              <li><a class="footer-link" href="#">Sign in</a></li>
-              <li><a class="footer-link" href="#">iOS app</a></li>
-              <li><a class="footer-link" href="#">Android app</a></li>
-            </ul>
-          </nav>
+      <nav class="nav-col">
+        <p class="footer-heading">Company</p>
+        <ul class="footer-nav">
+          <li><a class="footer-link" href="#">About HKT</a></li>
+          <li><a class="footer-link" href="#">For Business</a></li>
+          <li><a class="footer-link" href="#">Partners</a></li>
+          <li><a class="footer-link" href="#">Careers</a></li>
+        </ul>
+      </nav>
 
-          <nav class="nav-col">
-            <p class="footer-heading">Company</p>
-            <ul class="footer-nav">
-              <li><a class="footer-link" href="#">About HKT</a></li>
-              <li><a class="footer-link" href="#">For Business</a></li>
-              <li><a class="footer-link" href="#">Partners</a></li>
-              <li><a class="footer-link" href="#">Careers</a></li>
-            </ul>
-          </nav>
-
-          <nav class="nav-col">
-            <p class="footer-heading">Resources</p>
-            <ul class="footer-nav">
-              <li><a class="footer-link" href="#">Help center</a></li>
-              <li><a class="footer-link" href="#">Privacy & terms</a></li>
-            </ul>
-          </nav>
-        </div>
-      </footer>
+      <nav class="nav-col">
+        <p class="footer-heading">Resources</p>
+        <ul class="footer-nav">
+          <li><a class="footer-link" href="#">Help center</a></li>
+          <li><a class="footer-link" href="#">Privacy & terms</a></li>
+        </ul>
+      </nav>
     </div>
-  </div>
+  </footer>
 </body>
 
 </html>
