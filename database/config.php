bdbc416dfb7d9931643f323251d5ab67667f812b
Khanhtran47 <tranduckhanh23@gmail.com>
2021-12-20 01:02:06 +0100

    edit config file

--- database/config.php
+++ database/config.php
@@ -8,4 +8,4 @@ $cleardb_db = substr($cleardb_url["path"], 1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$mysqli = new mysqli($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
