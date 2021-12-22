c762bbf655c574a66f9dd4d9f7886044eca835fc
TRAN Duc Khanh <tranduckhanh23@gmail.com>
2021-12-22 19:57:43 +0100

    styled success page

--- pages/success/success.php
+++ pages/success/success.php
@@ -7,8 +7,7 @@ if (session_id() == '' || !isset($_SESSION)) {
 
 ?>
 
-<!doctype html>
-<html class="no-js" lang="en">
+<!DOCTYPE html>
 
 <head>
   <meta charset="UTF-8" />
@@ -18,59 +17,135 @@ if (session_id() == '' || !isset($_SESSION)) {
   <!-- Always include this line of code!!! -->
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-  <link rel="icon" href="img/favicon.png" />
-  <link rel="apple-touch-icon" href="img/apple-touch-icon.png" />
+  <link rel="icon" href="../../img/favicon.png" />
+  <link rel="apple-touch-icon" href="../../img/apple-touch-icon.png" />
   <link rel="manifest" href="manifest.webmanifest" />
   <link rel="preconnect" href="https://fonts.gstatic.com" />
   <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600;700&display=swap" rel="stylesheet" />
 
-  <link rel="stylesheet" href="../../css/style.css" />
-  <link rel="stylesheet" href="../../css/foundation.css" />
+  <link rel="stylesheet" href="../../css/grid.css" />
 
-  <link rel="stylesheet" href="../../css/general.css" />
-  <title>HKT Shop</title>
+  <link rel="stylesheet" href="success.css" />
+
+
+  <title>High Knowledge Technology</title>
 </head>
 
 <body>
 
-  <nav class="top-bar" data-topbar role="navigation">
-    <ul class="title-area">
-      <li class="name">
-        <h1><a href="../../index.php">HKT Shop</a></h1>
-      </li>
-      <li class="toggle-topbar menu-icon"><a href="#"><span></span></a></li>
-    </ul>
+  <body>
+    <header class="header" style="background : black">
+      <a href="../../index.php">
+        <img class="logo" alt="Genshin Logo" src="https://webstatic-sea.mihoyo.com/upload/event/2021/10/12/af8f45f5d1a34eb13aa2c70a2af59d05_6274939367807151451.png" />
+      </a>
 
-    <section class="top-bar-section">
-      <!-- Right Nav Section -->
-      <ul class="right">
-        <li><a href="../products/products.php">Products</a></li>
-        <li><a href="../cart/cart.php">View Cart</a></li>
-        <li><a href="../contact/contact.php">Contact</a></li>
-        <?php
+      <nav class="main-nav">
+        <ul class="main-nav-list">
+          <li><a href="../products/products.php" class="main-nav-link">Products</a></li>
+          <li><a href="../cart/cart.php" class="main-nav-link">View Cart</a></li>
+          <li><a href="../contact/contact.php" class="main-nav-link">Contact</a></li>
+          <?php
 
-        if (isset($_SESSION['username'])) {
-          echo '<li><a href="../orders/orders.php" class="main-nav-link">My Orders</a></li>';
-          echo '<li><a href="../myAccount/account.php">My Account</a></li>';
-          echo '<li><a href="../../components/logout.php">Log Out</a></li>';
-        } else {
-          echo '<li><a href="../login/login.php">Log In</a></li>';
-        }
-        ?>
-      </ul>
-    </section>
-  </nav>
+          if (isset($_SESSION['username'])) {
+            echo '<li><a href="../orders/orders.php" class="main-nav-link">My Orders</a></li>';
+            echo '<li><a href="../myAccount/account.php" class="main-nav-link">My Account</a></li>';
+            echo '<li><a href="../../components/logout.php" class="main-nav-link">Log Out</a></li>';
+          } else {
+            echo '<li><a href="../login/login.php" class="main-nav-link">Log In</a></li>';
+          }
+          ?>
+        </ul>
+      </nav>
 
-  <div class="row" style="margin-top:10px;">
-    <div class="small-12">
-      <p>Success. Whatever task you performed, has been executed successfully. Congrats!</p>
-      <p>In case you purchased a product, then please check your spam in email for the receipt.</p>
+      <button class="btn-mobile-nav">
+        <ion-icon class="icon-mobile-nav" name="menu-outline"></ion-icon>
+        <ion-icon class="icon-mobile-nav" name="close-outline"></ion-icon>
+      </button>
+    </header>
 
-      <footer style="margin-top:10px;">
-        <p style="text-align:center; font-size:0.8em;">&copy; HKT Shop. All Rights Reserved.</p>
-      </footer>
+    <div class="card">
+      <div style="border-radius:200px; height:200px; width:200px; background: #F8FAF5; margin:0 auto;">
+        <i class="checkmark">✓</i>
+      </div>
+      <h1>Success</h1>
+      <p>We received your purchase request.<br /> Please check your spam in email for the receipt.</p>
     </div>
-  </div>
-</body>
 
-</html>
+    <footer class="footer">
+      <div class="container grid grid--footer">
+        <div class="logo-col">
+          <a href="#" class="footer-logo">
+            <img class="logo" alt="HKGT logo" src="#" />
+          </a>
+
+          <ul class="social-links">
+            <li>
+              <a class="footer-link" href="#">
+                <ion-icon class="social-icon" name="logo-instagram"></ion-icon>
+              </a>
+            </li>
+            <li>
+              <a class="footer-link" href="#">
+                <ion-icon class="social-icon" name="logo-facebook"></ion-icon>
+              </a>
+            </li>
+            <li>
+              <a class="footer-link" href="#">
+                <ion-icon class="social-icon" name="logo-twitter"></ion-icon>
+              </a>
+            </li>
+          </ul>
+
+          <p class="copyright">
+            Copyright &copy; <span class="year">2027</span> by HKT, Inc. All rights reserved.
+          </p>
+        </div>
+
+        <div class="address-col">
+          <p class="footer-heading">Contact us</p>
+          <address class="contacts">
+            <p class="address">15 avenue Maréchal Foch, 41000 Blois, France</p>
+            <p>
+              <a class="footer-link" href="tel:+33 123 45 67 89">
+                +33 123 45 67 89
+              </a>
+              <br />
+              <a class="footer-link" href="mailto:hungc4k43@gmail.com">
+                hungc4k43@gmail.com
+              </a>
+            </p>
+          </address>
+        </div>
+
+        <nav class="nav-col">
+          <p class="footer-heading">Account</p>
+          <ul class="footer-nav">
+            <li><a class="footer-link" href="#">Create account</a></li>
+            <li><a class="footer-link" href="#">Sign in</a></li>
+            <li><a class="footer-link" href="#">iOS app</a></li>
+            <li><a class="footer-link" href="#">Android app</a></li>
+          </ul>
+        </nav>
+
+        <nav class="nav-col">
+          <p class="footer-heading">Company</p>
+          <ul class="footer-nav">
+            <li><a class="footer-link" href="#">About HKT</a></li>
+            <li><a class="footer-link" href="#">For Business</a></li>
+            <li><a class="footer-link" href="#">Partners</a></li>
+            <li><a class="footer-link" href="#">Careers</a></li>
+          </ul>
+        </nav>
+
+        <nav class="nav-col">
+          <p class="footer-heading">Resources</p>
+          <ul class="footer-nav">
+            <li><a class="footer-link" href="#">Help center</a></li>
+            <li><a class="footer-link" href="#">Privacy & terms</a></li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
+  </body>
+
+  </html>
